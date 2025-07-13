c9cc21c5fea6048e654cf0a7908d07924836f4f6
nenette <nenettediallo94@gmail.com>
2025-07-13 22:08:56 +0200

    Initial commit of my Node.js application

---
+++ app.js
@@ -0,0 +1,5 @@
+const direBonjour = require('./bonjour.js');
+const messagePourUtilisateur1 = direBonjour('Nene');
+console.log(messagePourUtilisateur1);
+console.log('\n--- Script app.js terminé ---');
+console.log('\n--- Script app.js terminé ---');
